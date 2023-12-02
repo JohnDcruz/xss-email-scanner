60390df868475f64d26dd12f9863c64d12d5f52e
JohnDcruz <johndcruz@johndcruz.com>
2023-12-02 00:18:38 -0500

    added basic implementation for gmail

--- background.js
+++ background.js
@@ -1,1 +1,36 @@
 console.log("Background script loaded!");
+
+const HTMLDomEvents = ["abort", "afterprint", "animationend", "animationiteration", "animationstart", "beforeprint", "beforeunload", "blur", "canplay", "canplaythrough", "change", "click", "contextmenu", "copy", "cut", "dblclick", "drag", "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop", "durationchange", "ended", "error", "focus", "focusin", "focusout", "fullscreenchange", "fullscreenerror", "hashchange", "input", "invalid", "keydown", "keypress", "keyup", "load", "loadeddata", "loadedmetadata", "loadstart", "message", "mousedown", "mouseenter", "mouseleave", "mousemove", "mouseover", "mouseout", "mouseup", "offline", "online", "open", "pagehide", "pageshow", "paste", "pause", "play", "playing", "popstate", "progress", "ratechange", "resize", "reset", "scroll", "search", "seeked", "seeking", "select", "show", "stalled", "storage", "submit", "suspend", "timeupdate", "toggle", "touchcancel", "touchend", "touchmove", "touchstart", "transitionend", "unload", "volumechange", "waiting", "wheel"];
+
+function checkLink(link) {
+  let linkHref = link.match(/href=".*?"/g);
+  if (linkHref) {
+    if (linkHref[0].includes("javascript:") || linkHref[0].includes("style=")) {
+      return true;
+    } else {
+      for (let i = 0; i < HTMLDomEvents.length; i++) {
+        if (linkHref[0].includes(("on" + HTMLDomEvents[i]))) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  let xssFound = false;
+  let xssLinks = [];
+
+  if (request.links) {
+    for (let i = 0; i < request.links.length; i++) {
+      if (checkLink(request.links[i])) {
+        xssFound = true;
+        xssLinks.push(request.links[i]);
+        break;
+      }
+    }
+  }
+
+  sendResponse({xssFound: xssFound, xssLinks: xssLinks});
+});
--- content.js
+++ content.js
@@ -1,1 +1,39 @@
 console.log("Content script loaded!");
+
+function getLinks(emailContents) {
+    let links = emailContents[0].innerHTML.match(/<a[^>]*>.*?<\/a>/g);
+
+    chrome.runtime.sendMessage({links: links}, function(response) {
+        if (response.xssFound) {
+          for (let i = 0; i < response.xssLinks.length; i++) {
+            let link = response.xssLinks[i];
+            // update the link text
+            let newLink = "<p>Malicious link detected: " + link + "</p>";
+            emailContents[0].innerHTML = emailContents[0].innerHTML.replace(link, newLink);
+          }
+        }
+    });
+}
+
+if (document.URL.includes("mail.google.com")) {
+    let emailContents = document.getElementsByClassName("gs");
+
+    // if page hasn't loaded yet, try again every 100ms for 10 seconds
+    if (emailContents.length == 0) {
+        let count = 0;
+        let interval = setInterval(function() {
+            emailContents = document.getElementsByClassName("gs");
+            if (emailContents.length > 0) {
+                clearInterval(interval);
+                getLinks(emailContents);
+            }
+            else if (count > 100) {
+                clearInterval(interval);
+            }
+            count++;
+        }, 100);
+    }
+    else {
+        getLinks(emailContents);
+    }
+}
