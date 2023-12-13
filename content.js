c90f359608fd1db465150466e9bb1fb6bb935e8b
JohnDcruz <johndcruz@johndcruz.com>
2023-12-12 21:01:08 -0500

    resolved issues with outlook

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
 console.log("Content script loaded!");
 
-function getLinks(emailContents) {
-    let links = emailContents[0].innerHTML.match(/<a[^>]*>.*?<\/a>/g);
-
+function getLinks(links, emailContents) {
     chrome.runtime.sendMessage({links: links}, function(response) {
         if (response.xssFound) {
           for (let i = 0; i < response.xssLinks.length; i++) {
             let link = response.xssLinks[i];
-            let newLink = "<p style=\"color:red;\">(Malicious link detected: " + link + ")</p>";
-            emailContents[0].innerHTML = emailContents[0].innerHTML.replace(link, newLink);
+            let linkText = link.substring(link.indexOf(">") + 1, link.indexOf("</a>"));
+            let linkHref = link.match(/href=".*?"/g);
+            let newLink = "<p style=\"color:red;\">(Malicious link detected: \"" + linkText + "\": " + linkHref + ")</p>";
+            emailContents.innerHTML = emailContents.innerHTML.replace(link, newLink);
           }
         }
     });
@@ -27,7 +27,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                 emailContents = document.getElementsByClassName("gs");
                 if (emailContents.length > 0) {
                     clearInterval(interval);
-                    getLinks(emailContents);
+                    getLinks(emailContents[0].innerHTML.match(/<a[^>]*>.*?<\/a>/g), emailContents[0]);
                 }
                 else if (count > 100) {
                     clearInterval(interval);
@@ -36,19 +36,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             }, 100);
         }
         else {
-            getLinks(emailContents);
+            getLinks(emailContents[0].innerHTML.match(/<a[^>]*>.*?<\/a>/g), emailContents[0]);
         }
       } else if (document.URL.includes("outlook.office.com")) {
         let emailContents = document.getElementById("UniqueMessageBody");
     
         // if page hasn't loaded yet, try again every 100ms for 10 seconds
-        if (emailContents.length == 0) {
+        if (emailContents == null) {
             let count = 0;
             let interval = setInterval(function() {
                 emailContents = document.getElementById("UniqueMessageBody");
-                if (emailContents.length > 0) {
+                if (emailContents != null) {
                     clearInterval(interval);
-                    getLinks(emailContents);
+                    getLinks(emailContents.innerHTML.match(/<a[^>]*>.*?<\/a>/g), emailContents);
                 }
                 else if (count > 100) {
                     clearInterval(interval);
@@ -57,7 +57,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             }, 100);
         }
         else {
-            getLinks(emailContents);
+            getLinks(emailContents.innerHTML.match(/<a[^>]*>.*?<\/a>/g), emailContents);
         }
       }
     }
